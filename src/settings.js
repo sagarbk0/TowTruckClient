f4d3f429bf042993e59e1d0a500b5ad46f12928c
Sagar Bhatt <60082886+sagarbk0@users.noreply.github.com>
2022-04-14 09:54:09 -0400

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,5 @@
 import Cookies from "universal-cookie";
 
-export const python_server = 'http://localhost:5000/';
+// export const python_server = 'http://localhost:5000/';
+export const python_server = 'https://sagarbk.pythonanywhere.com/'
 export const cookies = new Cookies();
